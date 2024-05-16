a5ef8e675e499f91ff7f4309d48cf173a4b90d22
Douglas Monteiro <do.uglas.14@hotmail.com>
2024-05-16 19:25:21 -0300

    Iniciando Projeto!

---
+++ js/products.js
@@ -0,0 +1,83 @@
+const products = [
+	{
+    id: 1,
+	titulo: "Moby dick",
+	preco: 15.99,
+	autor : "Sun tzu",
+	imagem: "https://cdn.awsli.com.br/600x1000/2088/2088593/produto/142516520/f563995353.jpg",
+	quantidade: 50,
+	categoria: "Historia"
+   },
+   {
+    id: 2,
+	titulo: "Simplesmente Acontece",
+	preco: 30.99,
+	autor : "Sun tzu",
+	imagem: "https://m.media-amazon.com/images/I/81Hu-hqqXtL._AC_UF1000,1000_QL80_.jpg",
+	quantidade: 50,
+	categoria: "Romance"
+   },
+   {
+    id: 3,
+	titulo: "Arte Da Guerra",
+	preco: 20.99,
+	autor : "Sun tzu",
+	imagem: "https://m.media-amazon.com/images/I/71DwE8vSyAL._AC_UF1000,1000_QL80_DpWeblab_.jpg",
+	quantidade: 50,
+	categoria: "auto-conhecimento"
+   },
+   {
+    id: 4,
+	titulo: "Moby dick",
+	preco: 15.99,
+	autor : "Sun tzu",
+	imagem: "https://cdn.awsli.com.br/600x1000/2088/2088593/produto/142516520/f563995353.jpg",
+	quantidade: 50,
+	categoria: "Historia"
+   },
+   {
+    id: 5,
+	titulo: "Simplesmente Acontece",
+	preco: 30.99,
+	autor : "Sun tzu",
+	imagem: "https://m.media-amazon.com/images/I/81Hu-hqqXtL._AC_UF1000,1000_QL80_.jpg",
+	quantidade: 50,
+	categoria: "Romance"
+   },
+   {
+    id: 6,
+	titulo: "Arte Da Guerra",
+	preco: 20.99,
+	autor : "Sun tzu",
+	imagem: "https://m.media-amazon.com/images/I/71DwE8vSyAL._AC_UF1000,1000_QL80_DpWeblab_.jpg",
+	quantidade: 50,
+	categoria: "auto-conhecimento"
+   },
+   {
+    id: 7,
+	titulo: "Moby dick",
+	preco: 15.99,
+	autor : "Sun tzu",
+	imagem: "https://cdn.awsli.com.br/600x1000/2088/2088593/produto/142516520/f563995353.jpg",
+	quantidade: 50,
+	categoria: "Historia"
+   },
+   {
+    id: 8,
+	titulo: "Simplesmente Acontece",
+	preco: 30.99,
+	autor : "Sun tzu",
+	imagem: "https://m.media-amazon.com/images/I/81Hu-hqqXtL._AC_UF1000,1000_QL80_.jpg",
+	quantidade: 50,
+	categoria: "Romance"
+   },
+   {
+    id: 9,
+	titulo: "Arte Da Guerra",
+	preco: 20.99,
+	autor : "Sun tzu",
+	imagem: "https://m.media-amazon.com/images/I/71DwE8vSyAL._AC_UF1000,1000_QL80_DpWeblab_.jpg",
+	quantidade: 50,
+	categoria: "auto-conhecimento"
+   },
+]
